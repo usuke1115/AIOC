b041d48d1f45e087026e29e8bc69b8a461e5e926
yuusuke <xiangzhongyoufu@gmail.com>
2024-10-14 05:10:37 +0900

    add: add copy and receive url

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Copy } from './components/copy';
 import { Home } from './components/home';
 import { Page404 } from './components/page404';
+import { Receive } from './components/receive';
 import './App.css';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/home' element={<Home />} />
+        <Route path='/copy' element={<Copy />} />
+        <Route path='/receive' element={<Receive />} />
         <Route path='*' element={<Page404 />} />
       </Routes>
     </BrowserRouter>
